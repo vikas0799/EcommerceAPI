704360700fece914fd14273b4778af0d926ae2b2
Vikas Patel <vikaspatel07071999@gmail.com>
2024-04-15 23:01:03 +0530

    Ecom Platform

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import User from './User'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Welcome to Ecom Platform</h1>
+      <User/>
+    </>
+  )
+}
+
+export default App
